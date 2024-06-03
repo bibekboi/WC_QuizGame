b1294143cf4866f6dc3519b10a9f3ae607605c3b
bibekboi <bibek.rizal1@gmail.com>
2024-06-03 17:11:39 +0545

    Final Game
    Game finalized.

--- scripts/global_scripts.gd
+++ scripts/global_scripts.gd
@@ -4,6 +4,11 @@ var score = 0
 var score_to_win = 8
 var time_elapsed
 
+var IS_MOUSE_PRESSED = false
+
+var pointyHandCursor = preload("res://assets/hand_point.png")
+var pointyHandCursor_pressed = preload("res://assets/hand_point_pressed.png")
+
 func _ready():
 	SilentWolf.configure({
 		"api_key": "qX98DtypRz8oLTy9as03G5B6ZrnlDj2o4qF7SrcG",
@@ -22,5 +27,15 @@ func _ready():
 		"reset_password_scene": "res://addons/silent_wolf/Auth/ResetPassword.tscn",
 		"session_duration_seconds": 0,
 		"saved_session_expiration_days": 30
-	})
+		})
+	
+	Input.set_custom_mouse_cursor(pointyHandCursor)
+
+func _process(delta):
+	if Input.is_action_just_pressed("click"):
+		Input.set_custom_mouse_cursor(pointyHandCursor_pressed)
+		
+	if Input.is_action_just_released("click"):
+		Input.set_custom_mouse_cursor(pointyHandCursor)
+
 
